a164ccab883f05d2f844ab51f278e6f1f10cd200
Eugene ZenBerry <eugeneberezhnoy@gmail.com>
2023-07-01 18:41:25 +0300

    It works, but the quotes are hard-coded

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
-// background.js
+// Will be used to open the settings in a new tab
 
-// Handle the browser action (clicking on the plugin's icon)
-chrome.browserAction.onClicked.addListener(function(tab) {
-  openOptionsPage(); // Open the options page in a new tab
-});
+// // Handle the browser action (clicking on the plugin's icon)
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   openOptionsPage(); // Open the options page in a new tab
+// });
 
-// Function to open the options page in a new tab
-function openOptionsPage() {
-  chrome.tabs.create({ url: chrome.runtime.getURL("options.html") });
-}
+// // Function to open the options page in a new tab
+// function openOptionsPage() {
+//   chrome.tabs.create({ url: chrome.runtime.getURL("options.html") });
+// }
--- content.js
+++ content.js
@@ -1,7 +1,16 @@
-// Get quotes from the extension settings
+// Get quotes from the extension settings – doesn't work for now
+// chrome.storage.sync.get({ quotes: '' }, function (items) {
+//   console.log(items.quotes);
+// })
 
 // Split quotes into an array
-const quoteArray = ['giraffe', 'mermaid', 'poodle'];
+const quoteArray = [
+'Check every method before you make changes to it: where else is it called? – my CTO',
+'Never leave empty fields in an object – My CTO',
+'Perfect calls to the server contain only an _id',
+'Always clear the fields when you copy code from somewhere #important',
+'Always check if you can use const instead of let'
+];
 
 // Filter out empty quotes
 const filteredQuotes = quoteArray.filter(quote => quote.trim() !== '');
@@ -14,16 +23,16 @@ const randomQuote = filteredQuotes[randomIndex];
 const quoteDiv = document.createElement('div');
 quoteDiv.setAttribute('id', 'quote-injector-div');
 quoteDiv.style.position = 'fixed';
-quoteDiv.style.top = '0';
-quoteDiv.style.left = '0';
-quoteDiv.style.width = '320px';
-quoteDiv.style.height = '240px';
+quoteDiv.style.top = Math.floor(Math.random() * (window.innerHeight-100)) + 'px'; // Randomize vertical position
+quoteDiv.style.left = Math.floor(Math.random() * (window.innerWidth-100)) + 'px'; // Randomize horizontal position
+// quoteDiv.style.width = '160px';
+// quoteDiv.style.height = '120px';
 quoteDiv.style.borderRadius = '20px';
-quoteDiv.style.backgroundColor = 'rgba(255, 204, 0, 0.3)';
-quoteDiv.style.color = '#fff';
+quoteDiv.style.backgroundColor = 'rgba(255, 204, 0, 1)';
+quoteDiv.style.color = '#000'; // Set text color to black
 quoteDiv.style.fontFamily = 'Arial, sans-serif';
-quoteDiv.style.fontSize = '14px';
-quoteDiv.style.padding = '10px';
+quoteDiv.style.fontSize = '16px';
+quoteDiv.style.padding = '30px';
 quoteDiv.style.zIndex = '9999';
 quoteDiv.textContent = randomQuote;
 quoteDiv.style.transition = "background-color 0.2s ease-in";
@@ -34,7 +43,7 @@ const closeButton = document.createElement("button");
 closeButton.style.position = "absolute";
 closeButton.style.top = "10px";
 closeButton.style.right = "10px";
-closeButton.style.color = "#fff";
+closeButton.style.color = "#000"; // Set close button color to black
 closeButton.style.border = "none";
 closeButton.style.backgroundColor = "transparent";
 closeButton.innerHTML = "&#10006;"; // Close symbol (X)
@@ -58,13 +67,13 @@ quoteDiv.addEventListener("mousedown", (event) => {
   };
 });
 
-quoteDiv.addEventListener("mouseover", (event) => {
-  quoteDiv.style.backgroundColor = 'rgba(255, 204, 0, 0.7)';
-});
+// quoteDiv.addEventListener("mouseover", (event) => {
+//   quoteDiv.style.backgroundColor = 'rgba(255, 204, 0, 1)';
+// });
 
-quoteDiv.addEventListener("mouseout", (event) => {
-  quoteDiv.style.backgroundColor = 'rgba(255, 204, 0, 0.3)';
-});
+// quoteDiv.addEventListener("mouseout", (event) => {
+//   quoteDiv.style.backgroundColor = 'rgba(255, 204, 0, 0.3)';
+// });
 
 quoteDiv.addEventListener("mouseup", () => {
   isDragging = false;
